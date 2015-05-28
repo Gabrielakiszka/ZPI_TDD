ffb136afcc04c445428fb89f88d0457299109215
Gabriela <Gabriela>
2015-05-28 19:49:34 +0200

    Test3: sprawdzenie istnienia atrybutu sum_numbers - test failed

--- tests.py
+++ tests.py
@@ -4,9 +4,15 @@ import unittest
 
 from exercise import DigitPowers
 
+
 class TestDigitPowers(unittest.TestCase):
+
     def test_digit_powers_exist(self):
         dp_result = DigitPowers()
+
+    def test_digit_powers_has_sum_numbers(self):
+        dp_result = DigitPowers()
+        assert dp_result.sum_numbers is not None, "dfp_result sum_numbers is None"
 
 if __name__ == "__main__":
     unittest.main()
