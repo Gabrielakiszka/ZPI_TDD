01821fa4d7602120f87e9084567577d1b4bb41f2
Gabriela <Gabriela>
2015-05-28 20:51:40 +0200

    Uporzadkowanie testow. Wykorzystanie metody setUp.

--- tests.py
+++ tests.py
@@ -8,24 +8,20 @@ from exercise import DigitPowers
 
 class TestDigitPowers(unittest.TestCase):
 
-    def test_digit_powers_exist(self):
-        dp_result = DigitPowers()
+    def setUp(self):
+        self.dp_result = DigitPowers()
 
     def test_digit_powers_has_sum_numbers(self):
-        dp_result = DigitPowers()
-        assert dp_result.sum_numbers is not None, "dfp_result sum_numbers is None"
+        assert self.dp_result.sum_numbers is not None, "dfp_result sum_numbers is None"
 
     def test_digit_powers_sum_of_powers(self):
-        dp_result = DigitPowers()
-        assert dp_result.sum_of_powers(1634, 4) == 1634, "dfp_result is not correct"
+        assert self.dp_result.sum_of_powers(1634, 4) == 1634, "dfp_result is not correct"
 
     def test_calc_max_value(self):
-        dp_result = DigitPowers()
-        assert dp_result.calc_max_value(2) == 243, "dfp_result is not correct"
+        assert self.dp_result.calc_max_value(2) == 243, "dfp_result is not correct"
 
     def test_solution(self):
-        dp_result = DigitPowers()
-        assert dp_result.solution(4) == 19316, "dfp_result is not correct"
+        assert self.dp_result.solution(4) == 19316, "dfp_result is not correct"
 
 if __name__ == "__main__":
     unittest.main()
