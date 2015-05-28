ff9c432b3262920b3e8f8fe04bd4ee2e527ab170
Gabriela <Gabriela>
2015-05-28 20:10:58 +0200

    Zaimplementowanie metody sum of powers do istniejacej klasy - test4 passed

--- exercise.py
+++ exercise.py
@@ -5,3 +5,9 @@ class DigitPowers():
     def __init__(self):
         self.sum_numbers = 0
 
+    @staticmethod
+    def sum_of_powers(number, power):
+        digits = [int(d) for d in str(number)]
+        return sum(d**power for d in digits)
+
+
