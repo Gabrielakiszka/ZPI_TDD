5dc9826048b6edba640e77a2b813f84436c50e4c
Gabriela <Gabriela>
2015-05-28 19:32:55 +0200

    Test1: sprawdzenie istnienia klasy DigitPowers-test failed.

---
+++ tests.py
@@ -0,0 +1,11 @@
+__author__ = 'Gabi'
+
+import unittest
+
+
+class TestDigitPowers(unittest.TestCase):
+    def test_digit_powers_exist(self):
+        dp_result = DigitPowers()
+
+if __name__ == "__main__":
+    unittest.main()
