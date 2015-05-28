5fb6223b48a7c48a8a4480ae1e0839c51015b89f
Gabriela <Gabriela>
2015-05-28 20:36:27 +0200

    Zaimplementowanie metody solution do istniejacej klasy - test6 passed

--- exercise.py
+++ exercise.py
@@ -17,6 +17,14 @@ class DigitPowers():
             start = start * 10 + 9
         return self.sum_of_powers(start, power)
 
+    def solution(self, power):
+        for number in range(10, self.calc_max_value(power)+1):
+            if self.sum_of_powers(number, power) == number:
+                self.sum_numbers += number
+        return self.sum_numbers
+
+
+
 
 
 
